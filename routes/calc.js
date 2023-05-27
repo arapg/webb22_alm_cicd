ccaf2ba0d77089d6de6df8d5619677f2fd80f067
arapg <hello@arpg.dev>
2023-05-27 15:59:31 +0200

    added subtraction functionality to calc.js

--- routes/calc.js
+++ routes/calc.js
@@ -40,14 +40,27 @@ router.get('/', (req, res, next) => {
 
 router.get('/:method', (req, res, next) => {
   const calculate = new Calculator();
+  const { method } = req.params;
 
   const val1 = parseInt(req.query.val[0], 10);
   const val2 = parseInt(req.query.val[1], 10);
+  let result = '';
 
-  calculate.add(Number(val1));
-  calculate.add(Number(val2));
+  switch (method) {
+    case add:
+      calculate.add(Number(val1));
+      calculate.add(Number(val2));
 
-  const result = `Adding ${val1} and ${val2} gives the sum ${calculate.getResult()}`;
+      result = `Adding ${val1} and ${val2} gives the sum ${calculate.getResult()}`;
+      break;
+    
+    case sub:
+      calculate.add(Number(val1));
+      calculate.subtract(Number(val2));
+
+      result = `Subtracting ${val2} from ${val1} gives the difference ${calculate.getResult()}`;
+      brea
+  }
 
   res.send(result);
 });
