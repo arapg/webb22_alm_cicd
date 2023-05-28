6bb3836bd1fc932e7e7812c0bbc9b5deb209f213
arapg <hello@arpg.dev>
2023-05-28 17:10:27 +0200

    added eslint-plugin-mocha and fixed lint errors

---
+++ test/calculator.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+const { Calculator } = require('../routes/calc');
+
+describe('Calculator', function () {
+  it('should return 5 when 2 and 3 are added', function () {
+    const calc = new Calculator();
+
+    calc.add(2);
+    calc.add(3);
+    assert.equal(calc.getResult(), 5);
+  });
+});
--- test/test.js
+++ test/test.js
@@ -1,21 +1,5 @@
-const assert = require('assert');
+describe('Dummy tests', function () {
+  it('should always pass', function () {
 
-const { Calculator } = require('../routes/calc');
-
-/* eslint-disable no-undef */
-
-describe('Dummy tests', () => {
-  it('should always pass', () => {
-
-  });
-});
-
-describe('Calculator', () => {
-  it('should return 5 when 2 and 3 are added', () => {
-    const calc = new Calculator();
-
-    calc.add(2);
-    calc.add(3);
-    assert.equal(calc.getResult(), 5);
   });
 });
